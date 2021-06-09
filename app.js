ba75215758744c47ec82c5a7a983f844a6e3e072
Baron C <cannonbaron@gmail.com>
2021-06-09 15:52:50 -0500

    added .when

--- app.js
+++ app.js
@@ -32,16 +32,30 @@ const promptUser = () => {
         if (nameInput) {
           return true;
         } else {
-          console.log('Please enter your name!');
+          console.log('Please enter your Github username!');
           return false;
         }
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself'
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
+
    ]);
 };
 
@@ -66,7 +80,7 @@ Add a New Project
         if (nameInput) {
           return true;
         } else {
-          console.log('Please enter your name!');
+          console.log('Please enter your project name!');
           return false;
         }
       }
@@ -79,7 +93,7 @@ Add a New Project
         if (nameInput) {
           return true;
         } else {
-          console.log('Please enter your name!');
+          console.log('Please enter your project description!');
           return false;
         }
       }
@@ -98,7 +112,7 @@ Add a New Project
         if (nameInput) {
           return true;
         } else {
-          console.log('Please enter your name!');
+          console.log('Please enter your Github link!');
           return false;
         }
       }
